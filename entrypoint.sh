795be3970d9da7370a46c0a42824e400ffc74764
shun198 <shunhiroseluvmri@gmail.com>
2022-10-22 16:18:29 +0900

    entrypoint.bashを変更

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ poetry run python manage.py makemigrations --noinput
 poetry run python manage.py migrate --noinput
 poetry run python manage.py collectstatic --noinput
 
-if [ $DEBUG = 1 ]; then
+if [ $DEBUG = "True" ]; then
     poetry run python manage.py runserver 0.0.0.0:8000
 else
     poetry run gunicorn djangopj.wsgi:application --bind 0.0.0.0:8000
