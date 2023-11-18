5a3803f20083598bd00d86b9f544839843bff56a
shun198 <shunhiroseluvmri@gmail.com>
2023-11-18 22:10:15 +0900

    👍 add delete and update tests

--- backend/application/tests/views/test_user.py
+++ backend/application/tests/views/test_user.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework import status
 
+from application.models.user import User
 from application.tests.factories.user import UserFactory
 
 
@@ -11,6 +12,16 @@ def get_user_url():
 
 def get_user_details_url(id):
     return f"/api/users/{id}/"
+
+
+@pytest.fixture
+def user_data():
+    return {
+        "employee_number": "11111111",
+        "username": "テストユーザ01",
+        "email": "test_user_01@test.com",
+        "role": User.Role.MANAGEMENT,
+    }
 
 
 @pytest.mark.django_db
@@ -93,3 +104,189 @@ def test_part_time_user_can_list_user_details(
     )
     response = client.get(get_user_details_url(user.id), format="json")
     assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_management_user_can_create_user(
+    client, management_user, user_password, get_user_url, user_data
+):
+    """管理者ユーザでユーザを作成できるテスト"""
+    client.login(
+        employee_number=management_user.employee_number, password=user_password
+    )
+    response = client.post(get_user_url, user_data, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_create_user(
+    client, general_user, user_password, get_user_url, user_data
+):
+    """一般ユーザでユーザを作成できないテスト"""
+    client.login(
+        employee_number=general_user.employee_number, password=user_password
+    )
+    response = client.post(get_user_url, user_data, format="json")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_create_user(
+    client, part_time_user, user_password, get_user_url, user_data
+):
+    """アルバイトユーザでユーザを作成できないテスト"""
+    client.login(
+        employee_number=part_time_user.employee_number, password=user_password
+    )
+    response = client.post(get_user_url, user_data, format="json")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_management_user_can_update_user(
+    client, management_user, user_password, user_data
+):
+    """管理者ユーザでユーザを更新できるテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=management_user.employee_number, password=user_password
+    )
+    response = client.put(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_update_user(
+    client, general_user, user_password, user_data
+):
+    """一般ユーザでユーザを更新できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=general_user.employee_number, password=user_password
+    )
+    response = client.put(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_update_user(
+    client, part_time_user, user_password, user_data
+):
+    """アルバイトユーザでユーザを更新できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=part_time_user.employee_number, password=user_password
+    )
+    response = client.put(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_management_user_can_partial_update_user(
+    client, management_user, user_password, user_data
+):
+    """管理者ユーザでユーザを一部更新できるテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=management_user.employee_number, password=user_password
+    )
+    response = client.patch(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_partial_update_user(
+    client, general_user, user_password, user_data
+):
+    """一般ユーザでユーザを一部更新できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=general_user.employee_number, password=user_password
+    )
+    response = client.patch(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_partial_update_user(
+    client, part_time_user, user_password, user_data
+):
+    """アルバイトユーザでユーザを一部更新できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=part_time_user.employee_number, password=user_password
+    )
+    response = client.patch(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_management_user_can_delete_user(
+    client, management_user, user_password, user_data
+):
+    """管理者ユーザでユーザを削除できるテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=management_user.employee_number, password=user_password
+    )
+    response = client.delete(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_delete_user(
+    client, general_user, user_password, user_data
+):
+    """一般ユーザでユーザを削除できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=general_user.employee_number, password=user_password
+    )
+    response = client.delete(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_general_user_cannot_delete_user(
+    client, part_time_user, user_password, user_data
+):
+    """アルバイトユーザでユーザを削除できないテスト"""
+    user = UserFactory()
+    client.login(
+        employee_number=part_time_user.employee_number, password=user_password
+    )
+    response = client.delete(
+        get_user_details_url(user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_user_cannot_delete_yourself(
+    client, management_user, user_password, user_data
+):
+    """自身を削除できないテスト"""
+    client.login(
+        employee_number=management_user.employee_number, password=user_password
+    )
+    response = client.delete(
+        get_user_details_url(management_user.id), user_data, format="json"
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"msg": "自身を削除する事は出来ません"}
