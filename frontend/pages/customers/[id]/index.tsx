8f76cc56e1653d97af5fa217f467675a189419d7
shun198 <shunhiroseluvmri@gmail.com>
2023-12-13 09:23:15 +0900

    👍 add icons

--- frontend/pages/customers/[id]/index.tsx
+++ frontend/pages/customers/[id]/index.tsx
@@ -1,6 +1,17 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import List from '@mui/material/List';
+import ListSubheader from "@mui/material/ListSubheader";
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+import EmailIcon from '@mui/icons-material/Email';
+import CakeIcon from '@mui/icons-material/Cake';
+import PersonIcon from '@mui/icons-material/Person';
+import HomeIcon from '@mui/icons-material/Home';
+import BadgeIcon from '@mui/icons-material/Badge';
+
 
 type CustomerDetailData = {
   id: number;
@@ -40,6 +51,9 @@ function CustomerDetail() {
         } else if (response.status === 403) {
           setLoggedIn(false);
           router.push("/"); // ログインしていない場合にルートページにリダイレクト
+        } else if (response.status === 404) {
+          setLoggedIn(false);
+          router.replace("/404"); // IDが存在しない場合は404ページへリダイレクト
         } else {
           alert("エラーが発生しました");
         }
@@ -63,16 +77,37 @@ function CustomerDetail() {
 
   return (
     <div className="customer-details">
-      <h1>お客様詳細</h1>
-      <div>{data.name}</div>
-      <div>{data.kana}</div>
-      <div>{data.birthday}</div>
-      <div>{data.email}</div>
-      <div>{data.phone_no}</div>
-      <div>{data.address}</div>
-      <div>{data.post_no}</div>
-      {/* 担当者 */}
-      <div>{data.updated_by}</div>
+      <List>        
+        <ListSubheader component="div" id="nested-list-subheader">
+            お客様詳細
+        </ListSubheader>
+        <ListItemIcon>
+            <PersonIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.name} />
+        <ListItemText primary={data.kana} />
+        <ListItemIcon>
+            <CakeIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.birthday} />
+        <ListItemIcon>
+            <EmailIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.email} />
+        <ListItemIcon>
+            <SmartphoneIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.phone_no} />
+        <ListItemIcon>
+            <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.address} />
+        <ListItemText primary={data.post_no} />
+        <ListItemIcon>
+            <BadgeIcon />
+        </ListItemIcon>
+        <ListItemText primary={data.updated_by} />
+      </List>
     </div>
   );
 }
